d2da862b0bcffac2fbe77534891db524c802fe6c
Ankitabit3496 <ankitachaudhary3496@gmail.com>
2021-05-05 22:03:20 +0530

    fake commit

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,9 +3,7 @@ const {calculateTip} = require('../src/math')
 test('should calculate total with tip',()=>{
  const total = calculateTip(10, .3)
  expect(total).toBe(13)
-//  if(total!==13){
-//      throw new Error(''+total)
-//  }
+
 })
 
 test('async test demo', (done)=>{
@@ -15,3 +13,4 @@ test('async test demo', (done)=>{
     },2000)
 })
 
+//////////////////
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,15 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+// const userOne = {
+//     name: 'tom',
+// }
+
+beforeEach(async ()=>{
+     await User.deleteMany()
+})
+
 
 test('should signup a new user', async()=>{
     await request(app).post('/users').send({
@@ -7,4 +17,11 @@ test('should signup a new user', async()=>{
         email: 'ank@g.com',
         password: 'red1234'
     }).expect(201)
+})
+
+test('should login existing user', async()=>{
+    await request(app).post('/users/login').send({
+        email:'',
+        password: ''
+    }).expect(200)
 })
